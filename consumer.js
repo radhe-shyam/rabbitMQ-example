65c58ba6a506cbe3087aeaa09d3a730ebe545e1a
radhe-shyam <jangir.radheyshyam@gmail.com>
2018-11-16 07:52:45 -0500

    with promises :alien:

--- consumer.js
+++ consumer.js
@@ -1,13 +1,20 @@
-var amqp = require('amqplib/callback_api');
+var amqp = require('amqplib');
+const queueName = 'hello';
+var channel, conn;
 
-amqp.connect('amqp://radhe:radhe@172.20.6.234:5672', function (err, conn) {
-    conn.createChannel(function (err, ch) {
-        var q = 'hello';
-
-        ch.assertQueue(q, { durable: true });
-        console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", q);
-        ch.prefetch(1);
-        ch.consume(q, function (msg) {
+amqp.connect('amqp://radhe:radhe@172.20.6.234:5672')
+    .then(function (connection) {
+        conn = connection;
+        return conn.createChannel();
+    })
+    .then(function (ch) {
+        channel = ch;
+        return channel.assertQueue(queueName, { durable: true });
+    })
+    .then(function (ok) {
+        console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", queueName);
+        channel.prefetch(1);
+        return channel.consume(queueName, function (msg) {
             var message = msg.content.toString();
             console.log(" [x] Received %s", message);
 
@@ -15,12 +22,13 @@ amqp.connect('amqp://radhe:radhe@172.20.6.234:5672', function (err, conn) {
                 console.log(message + i);
                 setTimeout(function () {
                     if (i > 20) {
-                        ch.ack(msg);
+                        channel.ack(msg);
                     } else
                         self(++i);
                 }, 1000);
             };
             self(1);
         }, { noAck: false });
+    }).catch(function (err) {
+        console.log('error =>', err);
     });
-});
--- producer.js
+++ producer.js
@@ -1,25 +1,27 @@
-var amqp = require('amqplib/callback_api');
+var amqp = require('amqplib');
+const queueName = 'hello';
+var channel, conn;
 
-amqp.connect('amqp://radhe:radhe@172.20.6.234:5672', function (err, conn) {
-    console.log('error connecting=>', err);
-    // console.log('connection done=>>', conn);
-    conn.createChannel(function (err, ch) {
-        console.log('error creating channel=>', err);
-        // console.log('channel created==>>', ch);
-
-        var q = 'hello';
-
-        ch.assertQueue(q, { durable: true });
+amqp.connect('amqp://radhe:radhe@172.20.6.234:5672')
+    .then(function (connection) {
+        conn = connection;
+        return conn.createChannel();
+    }).then(function (ch) {
+        channel = ch;
+        return channel.assertQueue(queueName, { durable: true });
+    }).then(function () {
         // Note: on Node 6 Buffer.from(msg) should be used
         var msg = process.argv.slice(2) || ['Hello', 'world'];
         msg.forEach(x => {
-            ch.sendToQueue(q, new Buffer(x), {
+            channel.sendToQueue(queueName, new Buffer(x), {
                 persistent: true
             });
             console.log(" [x] Sent '%s'", x);
         })
+
+        setTimeout(function () { console.log('closing the connection'); conn.close(); process.exit(0) }, 500);
+    }).catch(function (err) {
+        console.log('error =>', err);
     });
-    setTimeout(function () { conn.close(); process.exit(0) }, 500);
-});
 
 
